1fbdab066a4c9533fa23d95909af200cf5ec9300
Dan <49373281+HleinaD@users.noreply.github.com>
2024-11-26 17:04:39 +0100

    Update version number in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="SD4Py",
-    version="0.1.6",
+    version="0.1.7",
     author="Dan Hudson",
     author_email="daniel.dominic.hudson@uni-osnabrueck.de",
     packages=["sd4py"],
